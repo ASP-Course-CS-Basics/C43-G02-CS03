ec30de60c5d0f809a6753d8cd82a32ab241210ef
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-21 18:16:49 +0200

    Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,17 +5,36 @@
         static void Main(string[] args)
         {
             #region Q1
-            //int number;
-            //int.TryParse(Console.ReadLine(), out number);
+            //int number = int.Parse(Console.ReadLine());
             //string res = (number % 3 == 0 && number % 4 == 0) ? "Yes" : "No";
             //Console.WriteLine(res);
             #endregion
 
             #region Q2
-            //int num;
-            //int.TryParse(Console.ReadLine(), out num);
+            //int num = int.Parse(Console.ReadLine());
             //string res = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
             //Console.WriteLine(res);
+            #endregion
+
+            #region Q3
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            //int z = int.Parse(Console.ReadLine());
+
+            //int max = x;
+            //int min = x;
+
+            //if (y > max)
+            //    max = y;
+            //if (z > max)
+            //    max=z;
+
+            //if(y<min)
+            //    min = y;
+            //if(z<min)
+            //    min = z;
+
+            //Console.WriteLine($"Max Element = {max}\nMin Element = {min}");
             #endregion
         }
     }
