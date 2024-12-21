d98f896c9e91a202ba67cdafddfa7c34be68148a
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-21 18:00:28 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            int number;
+            int.TryParse(Console.ReadLine(), out number);
+            string res = (number % 3 == 0 && number % 4 == 0) ? "Yes" : "No";
+            Console.WriteLine(res);
+            #endregion
         }
     }
 }
