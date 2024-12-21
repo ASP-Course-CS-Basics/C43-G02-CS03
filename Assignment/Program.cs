32a80146d74278ece75557c8b8beff6289869f43
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-21 18:03:44 +0200

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,10 +5,17 @@
         static void Main(string[] args)
         {
             #region Q1
-            int number;
-            int.TryParse(Console.ReadLine(), out number);
-            string res = (number % 3 == 0 && number % 4 == 0) ? "Yes" : "No";
-            Console.WriteLine(res);
+            //int number;
+            //int.TryParse(Console.ReadLine(), out number);
+            //string res = (number % 3 == 0 && number % 4 == 0) ? "Yes" : "No";
+            //Console.WriteLine(res);
+            #endregion
+
+            #region Q2
+            //int num;
+            //int.TryParse(Console.ReadLine(), out num);
+            //string res = num > 0 ? "Positive" : num < 0 ? "Negative" : "Zero";
+            //Console.WriteLine(res);
             #endregion
         }
     }
