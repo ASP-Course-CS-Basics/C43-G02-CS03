4bedceaa24696a43c05c086a9ddbf974b4c205c1
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-21 19:59:30 +0200

    Q15

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -171,6 +171,27 @@
             //Console.WriteLine(result);
             #endregion
 
+            #region Q15
+            //int start = int.Parse(Console.ReadLine());
+            //int end = int.Parse(Console.ReadLine());
+            //for(int i = start; i <= end; i++)
+            //{
+            //int count = 0;
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        if(i%j == 0)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    if(count == 2)
+            //    {
+            //        Console.Write(i);
+            //        Console.Write(" ");
+            //    }
+            //}
+            #endregion
+
 
         }
     }
