259861d2b2e0f68be94696f30357bfc5553acb6d
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-22 17:01:31 +0200

    Part 02 Switch [Numeric Datatypes (Comparsion Operators)]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -17,14 +17,13 @@
             //    Console.WriteLine("Hello March!");
             //else
             //    Console.WriteLine("You enter monthNumber Not In 1st Quarte of year!");
-            //#endregion
             #endregion
 
             #region Switch Case [ Numeric Type - Equallity Comparison ]
             //Console.Write("Please enter month number exist in 1st Quarter of year: ");
             //int.TryParse(Console.ReadLine(), out int MonthNumber);
 
-            //switch(MonthNumber)
+            //switch (MonthNumber)
             //{
             //    case 1:
             //        Console.WriteLine("Hello Jan!");
@@ -43,12 +42,43 @@
 
             ///If Vs Switch
             ///*
-             /// * When you need to make condition on variable of type numeric 
-              ///* And compare Equallity (Case 1 , Case 2, .....)
-             /// * Use ( Switch Case ) For faster access elements, because compiler generate jumb table for this switch case.
+            /// * When you need to make condition on variable of type numeric 
+            ///* And compare Equallity (Case 1 , Case 2, .....)
+            /// * Use ( Switch Case ) For faster access elements, because compiler generate jumb table for this switch case.
             /// * */
             #endregion
 
+            #region Part 02 Switch [Numeric Datatypes (Comparsion Operators)]
+            #region Using ( If - Else )
+            //Console.Write("Enter Your Age : ");
+            //int.TryParse(Console.ReadLine(), out int Age);
+
+            //if (Age > 22)
+            //    Console.WriteLine("You're Greater Than 22.");
+            //else if (Age < 22)
+            //    Console.WriteLine("You're Less Than 22.");
+            //else
+            //    Console.WriteLine("You're 22."); 
+            #endregion
+
+            #region Using ( Switch - Case )
+            //Console.Write("Enter Your Age : ");
+            //int.TryParse(Console.ReadLine(), out int Age);
+
+            //switch (Age) 
+            //{
+            //    case > 22:
+            //        Console.WriteLine("You're Greater Than 22.");
+            //        break;
+            //    case < 22:
+            //        Console.WriteLine("You're Less Than 22.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You're 22.");
+            //        break;
+            //}
+            #endregion
+            #endregion
 
         }
     }
